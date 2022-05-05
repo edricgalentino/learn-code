32ac6239b89bdb01a9c724dfedc3ae0fa9b4ca27
edricgalentino <erdrikpertama@gmail.com>
2022-05-05 10:40:21 +0700

    c

--- Nodejs/contact-app/app.js
+++ Nodejs/contact-app/app.js
@@ -1,14 +1,14 @@
 // NPM Modules
 // Contact App
 
-const { makeQuestion, saveContact } = require("./contact");
+// const { makeQuestion, saveContact } = require("./contact");
 
-const getContacts = async () => {
-    const name = await makeQuestion("Write your name : ");
-    const email = await makeQuestion("Write your email : ");
-    const phoneNumber = await makeQuestion("Write your phone number : ");
+// const getContacts = async () => {
+//     const name = await makeQuestion("Write your name : ");
+//     const email = await makeQuestion("Write your email : ");
+//     const phoneNumber = await makeQuestion("Write your phone number : ");
 
-    saveContact(name, email, phoneNumber);
-};
+//     saveContact(name, email, phoneNumber);
+// };
 
-getContacts();
+// getContacts();
