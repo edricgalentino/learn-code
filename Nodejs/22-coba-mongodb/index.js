e698f8bae7cef445b30cead8b148824a00655449
edricgalentino <erdrikpertama@gmail.com>
2022-06-02 15:29:50 +0700

    add

---
+++ Nodejs/22-coba-mongodb/index.js
@@ -0,0 +1,9 @@
+const { MongoClient } = require("mongodb");
+
+const url = "mongodb://127.0.0.1:27017";
+const dbName = "coba-mongodb";
+
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
